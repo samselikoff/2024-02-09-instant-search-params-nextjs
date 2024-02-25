b653bd626775d677b6e0c5dbd2e51750272ff89d
Sam Selikoff <sam.selikoff@gmail.com>
2024-02-25 04:03:04 -0500

    wip

--- app/demo-2/genres-panel.tsx
+++ app/demo-2/genres-panel.tsx
@@ -22,27 +22,31 @@ export default function GenresPanel({ genres }: { genres: string[] }) {
   return (
     <div
       data-pending={pending ? "" : undefined}
-      className="bg-gray-700 rounded w-60 shadow-md shadow-gray-950/30 p-4"
+      className="bg-gray-700 rounded sm:w-60 shadow-md shadow-gray-950/30 p-4"
     >
-      {["1", "2", "3", "4", "5"].map((genre) => (
-        <label key={genre} className="flex gap-2 items-center">
-          <input
-            checked={optimisticGenres.includes(genre)}
-            onChange={(e) => {
-              let { name, checked } = e.target;
-              let newGenres = checked
-                ? [...optimisticGenres, name]
-                : optimisticGenres.filter((g) => g !== name);
-
-              updateGenres(newGenres);
-            }}
-            name={genre}
-            type="checkbox"
-            className="accent-blue-500"
-          />
-          Genre {genre}
-        </label>
-      ))}
+      <div className="flex gap-6 sm:block overflow-x-scroll pb-4 sm:pb-0 -mx-4 px-4 sm:-mx-0 sm:px-0">
+        {["1", "2", "3", "4", "5"].map((genre) => (
+          <label
+            key={genre}
+            className="flex gap-2 items-center whitespace-nowrap"
+          >
+            <input
+              checked={optimisticGenres.includes(genre)}
+              onChange={(e) => {
+                let { name, checked } = e.target;
+                let newGenres = checked
+                  ? [...optimisticGenres, name]
+                  : optimisticGenres.filter((g) => g !== name);
+                updateGenres(newGenres);
+              }}
+              name={genre}
+              type="checkbox"
+              className="accent-blue-500"
+            />
+            Genre {genre}
+          </label>
+        ))}
+      </div>
 
       <div className="mt-4">
         <button
